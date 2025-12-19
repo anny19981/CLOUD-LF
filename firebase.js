e74613d045099c0e90361acfff79ee3ab3f7f6a7
anny19981 <aholzer38@gmail.com>
2025-12-19 09:24:49 +0100

    Update firebase.js
    Datenbank einrichtung mit zugriff

--- firebase.js
+++ firebase.js
@@ -2,18 +2,11 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
-FileInputStream serviceAccount =
-new FileInputStream("path/to/serviceAccountKey.json");
-
-FirebaseOptions options = new FirebaseOptions.Builder()
-  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-  .build();
-
-FirebaseApp.initializeApp(options);
-
+import { getDatabase, onValue, ref } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyCIiCloSpQgL_sBHbDTAz0NXt8TqyoN17I",
   authDomain: "cloudfm-2a65a.firebaseapp.com",
+  databaseURL: "https://cloudfm-2a65a-default-rtdb.europe-west1.firebasedatabase.app", 
   projectId: "cloudfm-2a65a",
   storageBucket: "cloudfm-2a65a.firebasestorage.app",
   messagingSenderId: "249887318112",
@@ -25,3 +18,9 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+const db = getDatabase(); 
+const distanceRef = ref(db, 'user/' + userId + '/distance');
+onValue(distanceRef, (snapshot) => {
+  const data = snapshot,val();
+  updateDistance(postElement, data);
+});
